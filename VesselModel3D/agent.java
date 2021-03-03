6179903d02ced031717b6e6cf4655618d3d9461c
Alex <alxtaing@gmail.com>
2021-03-03 16:15:21 -0500

    Added Particle packing-- IT WORKS!
    particle packing implemented

--- VesselModel3D/agent.java
+++ VesselModel3D/agent.java
@@ -4,12 +4,14 @@ import HAL.GridsAndAgents.AgentPT3D;
 import HAL.GridsAndAgents.SphericalAgent3D;
 import HAL.Util;
 
+import java.util.Arrays;
+
 public class agent extends SphericalAgent3D<agent, grid3D> {
     ////////////////
     // PROPERTIES //
     ////////////////
 
-    int color;
+    int color = Util.WHITE;
     int type;
     int length = 0;
     int origin;
@@ -34,15 +36,86 @@ public class agent extends SphericalAgent3D<agent, grid3D> {
     public static int HEPARIN_MAP_COLOR = Util.RGB(48.0/255, 191.0/255, 217.0/255); // Heparin MAP;
     public static int MACROPHAGE_COLOR = Util.WHITE;
 
+    ///////////////////////
+    // UTILITY VARIABLES //
+    ///////////////////////
+
+    int[] posneg = {1, -1};
+    int[] MAP_init_hood = Util.SphereHood(true, grid3D.MAP_RAD); // EDIT LATER, ADD GAP?
 
     ////////////////////
     // INITIALIZATION //
     ////////////////////
 
-//    public void Init(int r) {
-//
-//    }
+    public void Recursive_MAP_Generator(){
+        assert G != null;
+        // recursive function, makes 12 surrounding MAP particles if they are in bounds and unoccupied
 
+        double x_distance = grid3D.MAP_GAP_CENTERS * Math.cos(Math.toRadians(60));
+        double y_distance = grid3D.MAP_GAP_CENTERS * Math.sin(Math.toRadians(60));
+
+        // Make 6 surrounding MAP_seed on same plane
+        for (int i : posneg) {
+            for (int j : posneg) {
+                // calculate new coordinate
+                double new_agent_x = Xpt() + (i * x_distance);
+                double new_agent_y = Ypt() + (j * y_distance);
+
+                // make sure that the new coordinate is inside bounds
+                boolean x_in_range = (0 < new_agent_x) && (new_agent_x < grid3D.x);
+                boolean y_in_range = (0 < new_agent_y) && (new_agent_y < grid3D.y);
+
+                // make sure that there is nothing there
+                boolean open_for_MAP = false;
+                if (x_in_range && y_in_range) {
+                    int occlusions = G.PopAt((int) new_agent_x, (int) new_agent_y, (int) Zpt());
+                    if (occlusions == 0) {
+                        open_for_MAP = true;
+                    }
+                }
+
+                if (x_in_range && y_in_range && open_for_MAP){
+                    agent new_agent = G.NewAgentPT(new_agent_x, new_agent_y, Zpt());
+                    new_agent.Recursive_MAP_Generator();
+                }
+            }
+        }
+
+        // 6 surrounding on either side of xy place.  The 3 "above" and 3 "below" in a triangular formation
+        // NOTE: np = next plane
+        // NOTE: sc = side cut
+        // see notes for details on calculations
+
+        double Zsc_distance = grid3D.MAP_GAP_CENTERS * Math.cos(Math.toRadians(30)); // distance above/below xy plane
+        double Ysc_distance = grid3D.MAP_GAP_CENTERS * Math.sin(Math.toRadians(30)); // distance above x coordinate
+
+        // calculate position of one that is straight forward (.'.) i.e. quotation mark
+
+        // calculate new coordinate
+        double new_agent_y = Ypt() + (Ysc_distance);
+        double new_agent_z = Zpt() + (Zsc_distance);
+
+        // make sure that the new coordinate is inside bounds
+        boolean y_in_range = (0 < new_agent_y) && (new_agent_y < grid3D.y);
+        boolean z_in_range = (0 < new_agent_z) && (Zpt() < grid3D.z);
+
+
+        // make sure that there is nothing there
+        boolean open_for_MAP = false;
+        if (y_in_range && z_in_range) {
+            int occlusions = G.PopAt((int) Xpt(), (int) new_agent_y, (int) new_agent_z);
+            if (occlusions == 0) {
+                open_for_MAP = true;
+            }
+        }
+
+        if (y_in_range && z_in_range && open_for_MAP){
+            agent new_agent = G.NewAgentPT(Xpt(), new_agent_y, new_agent_z);
+            System.out.println(Zpt());
+            new_agent.Recursive_MAP_Generator();
+        }
+
+    }
 
     /////////////
     // METHODS //
--- VesselModel3D/grid3D.java
+++ VesselModel3D/grid3D.java
@@ -17,7 +17,7 @@ public class grid3D extends AgentGrid3D<agent> {
     // MAP GEL
     public static final double HEPARIN_PERCENTAGE = 0.1; // enter as a decimal between 0 and 1
     public static final double MAP_RAD = 8;
-    public static double MAP_GAP = 3;
+    public static double MAP_GAP = 5;
 
 
     // DO NOT MODIFY
@@ -28,9 +28,9 @@ public class grid3D extends AgentGrid3D<agent> {
     public static final double divProb = 0.3;
 
     // GRID PROPERTIES
-    public static final int x = 500;
-    public static final int y = 500;
-    public static final int z = 500;
+    public static final int x = 200;
+    public static final int y = 200;
+    public static final int z = 200;
     public static final int SCALE_FACTOR = 2;
     public final static int TICK_PAUSE = 1;
     public final static int TIMESTEPS = 1000; // how long will the simulation run?
@@ -44,17 +44,19 @@ public class grid3D extends AgentGrid3D<agent> {
         OpenGL3DWindow window = new OpenGL3DWindow("Angiogenesis", 1000, 1000, x, y, z);
         grid3D woundGrid = new grid3D(x, y, z);
 
-        woundGrid.NewAgentPT(x/2.0, y/2.0, z/2.0).Init(MAP_RAD);
+        agent MAP_seed = woundGrid.NewAgentPTSafe(x * Math.random(), y * Math.random(), 0);
+        woundGrid.Init_MAP_Particles(MAP_seed);
+
 
         for (int step = 0; step < TIMESTEPS; step++) {
-            woundGrid.StepCells(divProb);
-            for (agent cell : woundGrid) {
-                if (woundGrid.rng.Double() < 0.1) {
-                    cell.Divide(20, woundGrid.rng);
-                }
-            }
-            woundGrid.DrawAgents(window);
+//            woundGrid.StepCells(divProb);
+//            for (agent cell : woundGrid) {
+//                if (woundGrid.rng.Double() < 0.1) {
+//                    cell.Divide(20, woundGrid.rng);
+//                }
+//            }
             woundGrid.DrawGrid(window);
+            woundGrid.DrawAgents(window);
         }
     }
 
@@ -84,26 +86,9 @@ public class grid3D extends AgentGrid3D<agent> {
     // INITIALIZATION //
     ////////////////////
 
-//    public void Init_MAP_Particles(){
-//        for (int i = 0; i < x*y; i++) {
-//            int cellType = agent.MAP_PARTICLE;
-//            double chance = Math.random();
-//            if (chance < HEPARIN_PERCENTAGE){
-//                cellType = agent.HEPARIN_ISLAND;
-//            }
-//            NewAgentPT(50, 50, 50);
-//
-//            int occlusions = MapOccupiedHood(MAP_space, i);
-//            int open = MapEmptyHood(MAP_rad, i);
-//            if (occlusions == 0) {
-//                for (int j = 0; j < open; j++){
-//                    if (0 < MAP_rad[j] && MAP_rad[j] < x*y){
-//                        NewAgentSQ(MAP_rad[j]).Init(cellType, false, 0);
-//                    }
-//                }
-//            }
-//        }
-//    }
+    public void Init_MAP_Particles(agent MAP_seed){
+        MAP_seed.Recursive_MAP_Generator();
+    }
 
     //////////////////
     // GRID ACTIONS //
@@ -122,19 +107,19 @@ public class grid3D extends AgentGrid3D<agent> {
     //////////////////
 
     public void DrawGrid (OpenGL3DWindow window){
-        for (int i=0; i < length; i++) {
+        window.ClearBox(Util.BLUE, Util.WHITE);
+//        for (int i=0; i < length; i++) {
 //            int color = Util.BLACK;
 //            if (GetAgent(i) != null){
 //                agent cell = GetAgent(i);
 //                color = cell.color;
 //            }
-        }
+//        }
     }
 
     public void DrawAgents(OpenGL3DWindow window){
-        window.ClearBox(Util.BLUE, Util.WHITE);
         for (agent cell : this) {
-            window.CelSphere(cell.Xpt(),cell.Ypt(),cell.Zpt(),MAP_RAD,Util.WHITE);
+            window.CelSphere(cell.Xpt(),cell.Ypt(),cell.Zpt(),10.0, cell.color);
         }
         window.Update();
     }
